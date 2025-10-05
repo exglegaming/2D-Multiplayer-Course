0f9a3b6581b7dfb0e2d1773b6335e7dd3019dc91
ExgleGaming <EaglesFan91x@outlook.com>
2025-10-04 23:44:24 -0400

    basic state synchronization and first rpc call

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -4,5 +4,11 @@ namespace DMultiplayerCourse.Scripts;
 
 public partial class Main : Node {
     public override void _Ready() {
+        RpcId(1, MethodName.PeerReady);
+    }
+
+    [Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
+    private void PeerReady() {
+        GD.Print($"Peer {Multiplayer.GetRemoteSenderId()} ready");
     }
 }
--- scripts/ui/MainMenu/MainMenu.cs
+++ scripts/ui/MainMenu/MainMenu.cs
@@ -1,36 +1,39 @@
 using Godot;
 
-namespace DMultiplayerCourse.Scripts.UI.main_menu;
+namespace DMultiplayerCourse.Scripts.UI.MainMenu;
 
 public partial class MainMenu : Control {
     private const int Port = 3000;
     
+    private PackedScene _mainScene;
     private Button _hostButton;
     private Button _joinButton;
     
     public override void _Ready() {
+        _mainScene = ResourceLoader.Load<PackedScene>("uid://j2rtu51dup7t");
         _hostButton = GetNode<Button>("%HostButton");
         _joinButton = GetNode<Button>("%JoinButton");
 
         _hostButton.Pressed += OnHostPressed;
         _joinButton.Pressed += OnJoinPressed;
-        Multiplayer.PeerConnected += OnPeerConnected;
+        Multiplayer.ConnectedToServer += OnConnectedToServer;
     }
 
     private void OnHostPressed() {
         var serverPeer = new ENetMultiplayerPeer();
         serverPeer.CreateServer(Port);
         Multiplayer.MultiplayerPeer = serverPeer;
+        GetTree().ChangeSceneToPacked(_mainScene);
     }
 
     private void OnJoinPressed() {
         var clientPeer = new ENetMultiplayerPeer();
         clientPeer.CreateClient("127.0.0.1", Port);
         Multiplayer.MultiplayerPeer = clientPeer;
+        
     }
 
-    private void OnPeerConnected(long id) {
-        GD.Print($"My peer id is: {Multiplayer.GetUniqueId()}");
-        GD.Print($"Peer connected {id}");
+    private void OnConnectedToServer() {
+        GetTree().ChangeSceneToPacked(_mainScene);
     }
 }
